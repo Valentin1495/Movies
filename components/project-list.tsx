3992e5addaa7a65770e70b39237cc4bb8e53cdd2
Valentin1495 <newface95@icloud.com>
2023-09-22 19:13:07 +0900

    Edit project description

--- components/project-list.tsx
+++ components/project-list.tsx
@@ -14,7 +14,8 @@ export default function ProjectList() {
       name: 'AIWanderlust',
       image: '/AI.jpg',
       href: 'https://aiwanderlust-noahhan.vercel.app',
-      description: '',
+      description:
+        'Custom Hook을 사용하여 만든 Multistep form을 통해 원하는 여행지와 활동을 제출합니다. Form을 제출하면, 사용자가 입력한 목적지의 경도와 위도를 Search parameters로 전달합니다. 이를 이용하여 Google Maps API를 호출하고, 해당 위치를 Google Maps에 표시합니다.  사용자가 선택한 활동도 Search parameters로 전달합니다. 여행지와 활동을 Prompt에 포함하여 PaLM API에 전송합니다. PaLM API는 이를 Request body로 하여 맞춤 여행 일정을 Response로 생성합니다. 특정 UI를 만들기 위해, Next.js에서 제공하는 파일들을 사용했습니다. e.g. layout.tsx, loading.tsx, not-found.tsx, error.tsx, etc',
     },
   ];
 
